a80dafb655a7a9722c3e533b0f55a4b4270a3be8
d1ll0n <dillon@indexed.finance>
2023-02-25 15:24:36 -0600

    splice test

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,9 @@
 import { ethers } from "hardhat";
-import { getScuffedContract } from "../src/scuffer";
+import { getScuffedContract, ScuffedContract } from "../src/scuffer";
 import { expect } from "chai";
 
 describe("", () => {
-  let contract: any;
+  let contract: ScuffedContract<any>;
   before(async () => {
     const f = await ethers.getContractFactory("TestContract");
     contract = getScuffedContract(await f.deploy());
@@ -11,7 +11,7 @@ describe("", () => {
 
   it("allowsBadCalldata", async () => {
     const scuff = contract.allowsBadCalldata({ val1: 100, val2: 1000 });
-    scuff.data.val1.replace(3000);
+    scuff.val1.replace(3000);
     expect(await scuff.call()).to.eq(3000);
   });
 
@@ -21,6 +21,15 @@ describe("", () => {
     const err = await scuff.call().catch((err) => {
       return err;
     });
-    expect(err.replacements).to.eq('Modification to: data.val1.tail @ byte 0:\n\tOld: 0x64\n\tReplacement: 0xbb8')
+    expect(err.replacements).to.eq(
+      "Modification to: data.val1.tail @ byte 0:\n\tOld: 0x64\n\tReplacement: 0xbb8"
+    );
+  });
+
+  it("ScuffedWriter.splice", async () => {
+    const scuff = contract.allowsBadCalldata({ val1: 100, val2: 1000 });
+    const removed = scuff.writer.spliceData(30, 2, "0xffff");
+    expect(removed).to.eq("0x0064");
+    expect(await scuff.call()).to.eq(0xffff);
   });
 });
